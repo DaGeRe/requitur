6dd187421aef2df132f28c258d65b7881e80bc5f
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2022-06-09 21:00:13 +0200

    Replace Sysout with log4j

--- src/main/java/de/dagere/requitur/ContentTraceExpander.java
+++ src/main/java/de/dagere/requitur/ContentTraceExpander.java
@@ -4,10 +4,15 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import de.dagere.requitur.content.Content;
 import de.dagere.requitur.content.RuleContent;
 
 public class ContentTraceExpander {
+   
+   private static final Logger LOG = LogManager.getLogger(ExistingRuleMarker.class);
 
    public static List<Content> expandContentTrace(final List<Content> trace, final Map<String, Rule> rules) {
       final List<Content> result = new LinkedList<>();
@@ -30,7 +35,7 @@ public class ContentTraceExpander {
             if (element.getValue() instanceof RuleContent) {
                final String value = ((RuleContent) element.getValue()).getValue();
                final Rule rule = rules.get(value);
-               System.out.println("Expanding: " + value);
+               LOG.trace("Expanding: ", value);
                final List<Content> expandedElements = expandTrace(rule.getElements(), rules);
                result.addAll(expandedElements);
             } else {
